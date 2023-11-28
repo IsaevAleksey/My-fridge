f9241d74567798a64adbb7b8b520d49328df3c47
Alexey Isaev <montilarard@gmail.com>
2023-11-28 19:03:10 +0300

    Initial Commit

---
+++ My fridge/My_fridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_fridgeApp.swift
+//  My fridge
+//
+//  Created by Алексей Исаев on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct My_fridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
